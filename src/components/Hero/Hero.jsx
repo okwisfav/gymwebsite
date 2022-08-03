a1378f521da5a3dcefaf298d9a1e7b18b4229633
ofego favour <ofegofavour@gmail.com>
2022-08-03 16:26:10 +0100

    hero seciton

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,8 +6,10 @@ const Hero = () => {
      <div className="hero">
          <div className="left-h">
             <Header />
-            <div className="the-best-ad"></div>
+            <div className="the-best-ad">
             <div></div>
+            <span>the best fitness club in the town </span>
+            </div>
          </div>
          <div className="right-h"></div>
      </div>
