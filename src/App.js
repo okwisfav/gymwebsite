509aaebec5970dc7df6105824c341f009362e779
ofego favour <ofegofavour@gmail.com>
2022-08-10 22:22:43 +0100

    Testimonials

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero';
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Testimorials from './components/Testimorials/Testimorials';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
         <Programs />
         <Reasons />
         <Plans />
+        <Testimorials />
     </div>
   );
 }
---
+++ src/components/Testimorials/Testimorials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Testimorials.css'
+const Testimorials = () => {
+  return (
+     <div className="Testimonials">
+        <div className="left-t">
+            <span>Testimonials</span>
+            <span>What they</span>
+            <span>say about us</span>
+        </div>
+        <div className="right-t"></div>
+     </div>
+  )
+}
+
+export default Testimorials
