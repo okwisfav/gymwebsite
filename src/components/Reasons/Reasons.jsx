ae062f2b5f56e40db194477027e7ba4fbc091298
ofego favour <ofegofavour@gmail.com>
2022-08-10 16:26:46 +0100

    Reason

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -26,21 +26,32 @@ const Reasons = () => {
             <div className='details-r'>
                <div>
                <img src={tick} alt=""></img>
-                <span></span>
+                <span>OVER 140+ EXPERT COACHS</span>
               </div>
               <div>
               <img src={tick} alt="" />
-                 <span></span>
+                 <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
                </div>
                <div>
                <img src={tick} alt="" />
-                  <span></span>
+                  <span>1 FREE PROGRAM FOR NEW MEMBER</span>
                </div>
                <div>
                <img src={tick} alt="" />
-                   <span></span>
+                   <span>RELIABLE PARTNERS</span>
                </div>
                </div>
+               <span style={{
+                  color : 'var(--gray)', 
+                  fontWeight: 'normal'
+               }}>
+                  OUR PARTNERS
+                  </span>
+                  <div className="partners">
+                     <img src={nb} alt="" />
+                     <img src={adidas} alt="" />
+                     <img src={nike} alt="" />
+                  </div>
           </div>
      </div>
   )
