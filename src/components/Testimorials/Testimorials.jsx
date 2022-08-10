a3cde1ab31bd8250f0012cae27df7c66eb5bfb1a
ofego favour <ofegofavour@gmail.com>
2022-08-10 22:30:08 +0100

    Textimorials

--- src/components/Testimorials/Testimorials.jsx
+++ src/components/Testimorials/Testimorials.jsx
@@ -5,7 +5,7 @@ const Testimorials = () => {
      <div className="Testimonials">
         <div className="left-t">
             <span>Testimonials</span>
-            <span>What they</span>
+            <span className="stroke-text">What they</span>
             <span>say about us</span>
         </div>
         <div className="right-t"></div>
