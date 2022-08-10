96caa38f45c2d6578a4c7cf93ed530c3c03c2db9
ofego favour <ofegofavour@gmail.com>
2022-08-10 17:06:45 +0100

    plans

--- src/components/Plans/Plans.jsx
+++ src/components/Plans/Plans.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {plansData} from '../../data/plansData'
 import './Plans.css';
+import whiteTick from '../../assets/whiteTick.png'
 const Plans = () => {
   return (
     <div className="plans-container">
@@ -11,7 +12,25 @@ const Plans = () => {
         </div>
         {/*plans card */}
         <div className="plans">
+           {plansData.map((plan, i)=>(
+              <div className="plan" key={1}>
+                {plan.icon}
+                <span>{plan.name}</span>
+                <span>${plan.price}</span>
 
+                <div className="features">
+                 {plan.features.map((feature, i)=>(
+                     <div className="feature">
+                        <img src={whiteTick} alt="" />
+                        <span key={1}>{feature}</span>
+                     </div>
+                  ))}
+                </div>
+                <div><span>see more benefits -></span>
+                </div>
+                <button className="btn">Join now</button>
+              </div>
+           ))} 
         </div>
     </div>
   )
